bcdf5003cfad667783301b84acac157cd15ffe20
Mike <mike@localhost.localdomain>
2015-05-07 16:02:34 -0700

    looked for timestamp to identify most recent post

--- CheckPage.py
+++ CheckPage.py
@@ -2,41 +2,49 @@ import urllib, urllib2, re
 from numpy import prod
 import string
 from time import sleep
+import easygui
 
 def CheckPage(last_post=None):
     req = urllib2.Request("https://www.facebook.com/centralcasting")
     response = urllib2.urlopen(req)
     page = response.read()
     
-    posts = re.split('<p>|</p>', page.replace('<br />',''))[1::2]
     posts = re.split('Central Casting Los Angeles', page.replace('<br />',''))
-    pposts = []
-    for p in posts[9:]:
-        pposts.append(string.join(re.split('<p>|</p>',p)[1::2]))
 
-    for p in pposts:
-        if p == last_post:
-            return None
-        pl = p.lower()
+    pposts = []
+    times = []
+    for p in posts[9:]:
+        #time = re.split('data-utime="|"',p)[2]
+        if 'data-utime="' in p:
+            times.append(int(p.split('data-utime="')[1].split('"')[0]))
+            pposts.append(string.join(re.split('<p>|</p>',p)[1::2]))
+
+    pposts = [x for (y,x) in sorted(zip(times,pposts))]
+
+    most_recent = pposts[-1]
+
+    if most_recent == last_post:
+        return None
+    else:
+        pl = most_recent.lower()
         nonunion = "non union" in pl
         sex = "female" in pl or "women" in pl
         ethnicity = "ethnicity" in pl or "caucasian" in pl
-
-        if nonunion and sex and ethnicity:
-            return p
+#        if nonunion and sex and ethnicity:
+        return most_recent
 
     return None
 
 def main():
     post = None
     while True:
-        print "Checking page...\n"
         newpost = CheckPage(post)
         if newpost != post and newpost != None:
-            print newpost + "\n"
-            post= newpost
-        else:
-            print "No new posts.\n"
+            easygui.msgbox(newpost, title="Casting call!")
+            with open("calls.txt", "a") as f:
+                f.write(newpost)
+                f.write("\n")
+            post = newpost
         sleep(1)
         
 
